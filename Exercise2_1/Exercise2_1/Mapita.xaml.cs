b44cab3194148db48b85b3abdb291600a708f803
lissien <lissym906@gmail.com>
2022-07-15 13:10:16 -0600

    subida

--- Exercise2_1/Exercise2_1/Mapita.xaml.cs
+++ Exercise2_1/Exercise2_1/Mapita.xaml.cs
@@ -20,7 +20,50 @@ namespace Exercise2_1
         {
             InitializeComponent();
         }
-        
-        
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
+            String NombreP = txtNombre.Text;
+            String CapitalP = txtCapital.Text;
+            double Latitud = Convert.ToDouble(txtLatitud.Text);
+            double Longitud = Convert.ToDouble(txtLongitud.Text);
+            String Mmoneda = txtMoneda.Text;
+            String Lang = txtidioma.Text;
+            //CREANDO EL OBJETO PIN
+            Pin pin = new Pin
+            {
+                Label = "Pais: " + NombreP + " Capital: " + CapitalP,
+                Address = "Moneda: " + Mmoneda + " Lenguaje: " + Lang,
+                Type = PinType.Generic,
+                Position = new Position(Latitud, Longitud)
+            };
+            Maps.Pins.Add(pin);
+            //MOVERSE A LA REGION DE LA LOCALIZACION OBTENIDA
+            var location = await Geolocation.GetLocationAsync();
+            if (location == null) { location = await Geolocation.GetLastKnownLocationAsync(); }
+            Maps.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(Latitud, Longitud), Distance.FromMiles(1)));
+
+            //ACTUALIZAR LA UBICACION Y PONERSE A LA ESCUCHA DE CUANDO VOLVER A LA MISMA LOCALIZACION
+            Plugin.Geolocator.Abstractions.IGeolocator geotoCelphone = CrossGeolocator.Current;
+            if (geotoCelphone != null)
+            {
+                geotoCelphone.PositionChanged += Locatilazion_PositionChanged;
+                if (!geotoCelphone.IsListening)
+                {
+                    await geotoCelphone.StartListeningAsync(TimeSpan.FromSeconds(10), 120);
+                }
+                await geotoCelphone.GetPositionAsync();
+                Maps.MoveToRegion(MapSpan.FromCenterAndRadius(new Position(Latitud, Longitud), Distance.FromMiles(1)));
+            }
+            else
+            {
+                await geotoCelphone.GetLastKnownLocationAsync();
+                await geotoCelphone.GetPositionAsync();
+                Maps.MoveToRegion(new MapSpan(new Position(Latitud, Longitud), 1, 1));
+            }
+        }
+
+
     }
 }
