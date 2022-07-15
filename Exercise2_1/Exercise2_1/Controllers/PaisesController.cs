e6ad311295b083287ad8f0a42c2c983977b93a68
AlexanderJoker <alexandertinoco12349@gmail.com>
2022-07-15 13:31:51 -0600

    Subido PaisesController

--- Exercise2_1/Exercise2_1/Controllers/PaisesController.cs
+++ Exercise2_1/Exercise2_1/Controllers/PaisesController.cs
@@ -10,7 +10,97 @@ namespace Exercise2_1.Controllers
 {
     public class PaisesController
     {
-        
+        public async static Task<List<Paises.Pais>> getPaisesA()
+        {
+            List<Paises.Pais> listaPaises = new List<Paises.Pais>();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://restcountries.com/v3.1/region/America");
+                if (response.IsSuccessStatusCode)
+                {
+                    var contenido = response.Content.ReadAsStringAsync().Result;
+                    listaPaises = JsonConvert.DeserializeObject<List<Paises.Pais>>(contenido);
+
+                }
+            }
+            return listaPaises;
+        }
+        public async static Task<List<Paises.Pais>> getPaisesE()
+        {
+            List<Paises.Pais> listaPaises = new List<Paises.Pais>();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://restcountries.com/v3.1/region/Europe");
+                if (response.IsSuccessStatusCode)
+                {
+                    var contenido = response.Content.ReadAsStringAsync().Result;
+                    listaPaises = JsonConvert.DeserializeObject<List<Paises.Pais>>(contenido);
+
+                }
+            }
+            return listaPaises;
+        }
+        public async static Task<List<Paises.Pais>> getPaisesAf()
+        {
+            List<Paises.Pais> listaPaises = new List<Paises.Pais>();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://restcountries.com/v3.1/region/Africa");
+                if (response.IsSuccessStatusCode)
+                {
+                    var contenido = response.Content.ReadAsStringAsync().Result;
+                    listaPaises = JsonConvert.DeserializeObject<List<Paises.Pais>>(contenido);
+
+                }
+            }
+            return listaPaises;
+        }
+        public async static Task<List<Paises.Pais>> getPaisesAs()
+        {
+            List<Paises.Pais> listaPaises = new List<Paises.Pais>();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://restcountries.com/v3.1/region/Asia");
+                if (response.IsSuccessStatusCode)
+                {
+                    var contenido = response.Content.ReadAsStringAsync().Result;
+                    listaPaises = JsonConvert.DeserializeObject<List<Paises.Pais>>(contenido);
+
+                }
+            }
+            return listaPaises;
+        }
+        public async static Task<List<Paises.Pais>> getPaisesOs()
+        {
+            List<Paises.Pais> listaPaises = new List<Paises.Pais>();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://restcountries.com/v3.1/region/Oceania");
+                if (response.IsSuccessStatusCode)
+                {
+                    var contenido = response.Content.ReadAsStringAsync().Result;
+                    listaPaises = JsonConvert.DeserializeObject<List<Paises.Pais>>(contenido);
+
+                }
+            }
+            return listaPaises;
+        }
+        //--ESTA ULTIMA ES USADA PARA FACILITAR LA OBTENCION DE ALGUNOS DATOS del pin del mapa
+        public async static Task<List<Pais2>> getOnePais(String nom)
+        {
+            List<Pais2> listaPais = new List<Pais2>();
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://restcountries.com/v2/name/" + nom);
+                if (response.IsSuccessStatusCode)
+                {
+                    var contenido = response.Content.ReadAsStringAsync().Result;
+                    listaPais = JsonConvert.DeserializeObject<List<Pais2>>(contenido);
+                }
+            }
+            return listaPais;
+        }
+
     }
 
 }
